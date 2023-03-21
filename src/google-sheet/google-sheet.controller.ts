e03810fd037c30a4240e43d6bf3a37e8501f952a
riyasharma18 <sharmariyasharma1039@gmail.com>
2023-03-21 19:04:59 +0530

    data logging

--- src/google-sheet/google-sheet.controller.ts
+++ src/google-sheet/google-sheet.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { HttpService } from '@nestjs/axios';
 
-@Controller('google-sheet')
-export class GoogleSheetController {}
+@Controller('api/v1/google-sheet')
+export class GoogleSheetController {
+  constructor(private httpService: HttpService) {}
+  @Get('/fetch-data')
+  async getSheetData(): Promise<any> {
+    const sheetUrl =
+      'https://docs.google.com/spreadsheets/d/1dv75i9VLnIJXCybO3b92qCWB8BgaS0wNAlSz6W-ZzkU/export?format=csv';
+    const response = await this.httpService.get(sheetUrl).toPromise();
+    const csvData = response.data;
+    console.log(csvData); // or use a logger instance
+    return { message: 'Data logged!' };
+  }
+}
--- src/google-sheet/google-sheet.module.ts
+++ src/google-sheet/google-sheet.module.ts
@@ -1,8 +1,10 @@
+import { HttpModule } from '@nestjs/axios/dist/http.module';
 import { Module } from '@nestjs/common';
 import { GoogleSheetController } from './google-sheet.controller';
 import { GoogleSheetService } from './google-sheet.service';
 
 @Module({
+  imports: [HttpModule],
   controllers: [GoogleSheetController],
   providers: [GoogleSheetService],
 })
