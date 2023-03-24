4301394ea5e0a65056ed266ba962f3ca457cd12d
riyasharma18 <sharmariyasharma1039@gmail.com>
2023-03-24 09:00:42 +0530

    fix: asana service

--- src/asana/asana.service.ts
+++ src/asana/asana.service.ts
@@ -27,4 +27,30 @@ export class AsanaService {
     );
     return response.data;
   }
+
+  async getTasks(projectId: string): Promise<any> {
+    const headers = {
+      Authorization: `Bearer ${this.ASANA_ACCESS_TOKEN}`,
+      'Content-Type': 'application/json',
+    };
+
+    const response = await axios.get(
+      `${this.ASANA_API_BASE_URL}/tasks?project=${projectId}`,
+      { headers },
+    );
+    return response.data;
+  }
 }
+
+const asanaService = new AsanaService();
+const projectId = '1204172907154852';
+const projectId2 = '1204103455728246';
+const taskName = 'Create Asana Controller';
+asanaService
+  .createTask(projectId, taskName)
+  .then((result) => {
+    console.log(result);
+    return asanaService.getTasks(projectId2);
+  })
+  .then((tasks) => console.log(tasks))
+  .catch((error) => console.error(error));
