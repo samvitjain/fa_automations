be1d7363e8dbc93da7150da2424af0e560a34edc
Riya Sharma <60288671+riyasharma18@users.noreply.github.com>
2023-03-25 15:17:12 +0530

    feat: migrate code into nest js (#4)
    * fix issues
    
    * shift project into nestjs

--- src/asana/asana.service.ts
+++ src/asana/asana.service.ts
@@ -1,15 +1,14 @@
 import { Injectable } from '@nestjs/common';
 import axios from 'axios';
-
+import * as dotenv from 'dotenv';
+dotenv.config();
 @Injectable()
 export class AsanaService {
-  private readonly ASANA_ACCESS_TOKEN =
-    '1/1190264422161000:12ed156cf7166ab978ec91cd40f0ce53';
   private readonly ASANA_API_BASE_URL = 'https://app.asana.com/api/1.0';
 
   async createTask(projectId: string, taskName: string): Promise<any> {
     const headers = {
-      Authorization: `Bearer ${this.ASANA_ACCESS_TOKEN}`,
+      Authorization: `Bearer ${process.env.ASANA_ACCESS_TOKEN}`,
       'Content-Type': 'application/json',
     };
 
@@ -30,7 +29,7 @@ export class AsanaService {
 
   async getTasks(projectId: string): Promise<any> {
     const headers = {
-      Authorization: `Bearer ${this.ASANA_ACCESS_TOKEN}`,
+      Authorization: `Bearer ${process.env.ASANA_ACCESS_TOKEN}`,
       'Content-Type': 'application/json',
     };
 
