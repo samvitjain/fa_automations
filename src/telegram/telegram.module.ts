acba0884c36d61f2c0467de4858b64899fb07536
Samvit Jain <jain.samvit@gmail.com>
2023-03-28 17:12:15 +0530

    fix error

--- src/telegram/telegram.module.ts
+++ src/telegram/telegram.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AsanaProject } from 'src/entities/asana-project.entity';
+import { FAUser } from 'src/entities/fa-user.entity';
 import { TelegramController } from './telegram.controller';
 import { TelegramService } from './telegram.service';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([FAUser, AsanaProject])],
   controllers: [TelegramController],
-  providers: [TelegramService]
+  providers: [TelegramService],
+  exports: [TypeOrmModule],
 })
 export class TelegramModule {}
